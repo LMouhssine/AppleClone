27c377edef612b7ef1709727114fafaf9dc1b396
Mouhssine Lakhili <mouhssine.lakhili@yahoo.com>
2024-05-12 17:17:52 +0200

    Swiper JS & Photos

---
+++ script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  centeredSlides: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+});
